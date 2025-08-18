0e70ce4eb2657838342a40e7e5b0c244724f06e5
ilax666 <thierryverne41@gmail.com>
2025-08-18 21:55:15 +0200

    Update argile.v

--- argile/argile.v
+++ argile/argile.v
@@ -796,3 +796,31 @@ struct Clay_Context {
     dynamic_string_data                      []u8
     debug_element_data                       []Clay_DebugElementData
 }
+
+
+/*
+LICENSE
+zlib/libpng license
+
+Copyright (c) 2024-present Nic Barker (original author)
+Copyright (c) 2025-present ilax666 (port to V)
+
+This software is provided 'as-is', without any express or implied warranty.
+In no event will the authors be held liable for any damages arising from the
+use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software in a
+    product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not
+    be misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source
+    distribution.
+*/
